790530cd39bb67124d1a19a11357d8e71e5f7f3b
sonu susan varughese <sonususanvarghese.99@gmail.com>
2026-03-27 21:52:43 +0530

    change name

--- client/src/components/series.jsx
+++ client/src/components/series.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Movies.css";
+import "./movies.css";
 
 function Series() {
   const [series, setSeries] = useState([]);
