61b35d463001707fc024420e50474eaabbd5eb82
sonu susan varughese <sonususanvarghese.99@gmail.com>
2026-03-27 21:33:08 +0530

    fix css path

--- client/src/components/movies.jsx
+++ client/src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Movies.css";
+import "./movies.css";
 
 function Movies() {
   const [movies, setMovies] = useState([]);
